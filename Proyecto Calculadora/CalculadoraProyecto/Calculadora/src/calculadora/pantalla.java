0b1958d4a3a1f890a4ad796042dde6b1d6dec03e
Cristhian Alvarez <gerardoalvares354@gmail.com>
2024-08-21 22:51:38 -0600

    Add files via upload

---
+++ Proyecto Calculadora/CalculadoraProyecto/Calculadora/src/calculadora/pantalla.java
@@ -0,0 +1,15 @@
+package calculadora;
+class pantalla 
+{
+
+    static String getText() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setText(String Total) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
